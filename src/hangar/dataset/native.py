a82412d654dd14d0d25b9ee4ec464082ddc0fd28
hhsecond <sherin@tensorwerk.com>
2020-03-16 20:16:54 +0530

    more checks on the keys

--- src/hangar/dataset/native.py
+++ src/hangar/dataset/native.py
@@ -3,7 +3,6 @@ import numpy as np
 
 
 # TODO: document that Dataset can only load ndarray
-# TODO: Work on returning nested arrays
 
 def data_process_decorator(f, processor):
     @wraps(f)
@@ -13,16 +12,18 @@ def data_process_decorator(f, processor):
 
 
 class DataAccessor:
-    def __init__(self, column):
-        if isinstance(column, tuple):
-            process_data = column[1]
-            column = column[0]
-            self.get_by_name = data_process_decorator(self.get_by_name, process_data)
+    def __init__(self, column, data_process_func=None):
+        if data_process_func:
+            self.get_by_name = data_process_decorator(self._get_by_name_impl,
+                                                      data_process_func)
+        else:
+            self.get_by_name = self._get_by_name_impl
+        # TODO: check the type of column
         self._be_fs = column._be_fs
         self._samples = column._samples
         self.schema_type = column.schema_type
 
-    def get_by_name(self, name):
+    def _get_by_name_impl(self, name):
         try:
             spec = self._samples[name]
             return self._be_fs[spec.backend].read_data(spec)
@@ -36,13 +37,37 @@ class Dataset:
         if columns is None:
             return
         accessors = []
-        for c in columns:
-            accessors.append(DataAccessor(c))
-        if keys is None:
-            first = columns[0]
-            if isinstance(first, (tuple, list)):
-                first = first[0]
-            keys = list(first.keys())
+        all_keys = []
+        all_remote_keys = []
+        for col in columns:
+            if isinstance(col, (tuple, list)):
+                data_process_func = col[1]
+                col = col[0]
+                acc = DataAccessor(col, data_process_func)
+            else:
+                acc = DataAccessor(col)
+            accessors.append(acc)
+            all_keys.append(set(col.keys()))
+            all_remote_keys.append(set(col.remote_reference_keys))
+        common_keys = set.intersection(*all_keys)
+        remote_keys = set.union(*all_remote_keys)
+        common_local_keys = common_keys.difference(remote_keys)
+
+        if keys:
+            unique = set(keys)
+            notCommon = unique.difference(common_keys)
+            notLocal = unique.difference(common_local_keys)
+
+            # TODO: These error message could eat up the whole terminal space if the size of
+            #   non common and non local keys are huge
+            if len(notCommon) > 0:
+                raise KeyError(f'Keys: {notCommon} do not exist in all columns.')
+            if len(notLocal) > 0:
+                raise FileNotFoundError(
+                    f'Keys: {notLocal} are remote data samples not downloaded locally.')
+        else:
+            keys = tuple(common_local_keys)
+        # TODO: batch_size 1 means returning one sample with batch dimension, we don't need that
         batch_size = 1
         num_batches = len(keys)
         self._setup(accessors, keys, batch_size, num_batches)
@@ -51,7 +76,6 @@ class Dataset:
         self.accessors = accessors
         self.batch_size = batch_size
         self.num_batches = num_batches
-        self.num_batches = len(keys)
         self.keys = keys
 
     @classmethod
@@ -84,24 +108,19 @@ class Dataset:
         return make_tf_dataset(self.accessors, self.keys)
 
     def __getitem__(self, item):
-        # TODO: return namedtuples if found useful
         return tuple([acc.get_by_name(item) for acc in self.accessors])
 
     def __iter__(self):
         start = 0
         end = self.batch_size
-        batched_keys = []
         for i in range(self.num_batches):
-            batched_keys.append(self.keys[start:end])
+            batch = self.keys[start:end]
             start = end
             end = end + self.batch_size
-
-        for batch in batched_keys:
             out = []
             for accessor in self.accessors:
+                data = [accessor.get_by_name(name) for name in batch]
                 # TODO: np stack or filling empty array
-                out.append(
-                    np.stack(
-                        [accessor.get_by_name(name) for name in batch]))
+                out.append(np.stack(data))
             yield tuple(out)
 
